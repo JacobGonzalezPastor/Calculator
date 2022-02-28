07022c2de544c353c7aef3cac704b55906cf7dee
Jacob Gonzalez <jacobenrrique@hotmial.com>
2022-02-27 21:02:32 -0500

    cal

---
+++ src/container/App.js
@@ -0,0 +1,12 @@
+import Calculadora from '../components/Calculadora';
+import '../style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculadora/>
+    </div>
+  );
+}
+
+export default App;
